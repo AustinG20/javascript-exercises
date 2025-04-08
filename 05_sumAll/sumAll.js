c267beda1c83d82b0e23ea5f56c4417879660f29
Austin <agobby471@gmail.com>
2025-04-08 19:07:15 -0400

    Comeplete exercises 5 to 7 for javascript

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,35 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let total = 0;
 
+    if(start < 0 || end < 0 || typeof(start) != 'number' || typeof(end) != 'number' || start % 1 != 0 || end % 1 != 0){
+        return 'ERROR';
+    }else{
+        if(start < end){
+            if(((end-start) % 2) === 0){
+                total = start;
+                start++;
+            }
+            
+            while((start != end) && (start < end)){
+                total += start + end;
+                start++;
+                end--;
+            }
+        }else{
+            if(((start-end) % 2) === 0){
+                total = end;
+                end++;
+            }
+            
+            while((start != end) && (start > end)){
+                total += start + end;
+                end++;
+                start--;
+            }
+        }
+    }
+
+    return total;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let celcius = 0;
+
+  celcius = (temp - 32) * 5/9;
+
+  celcius = parseFloat(celcius.toFixed(1));
+
+  return celcius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let fahrenheit = 0;
+
+  fahrenheit = temp * (9/5) + 32;
+
+  fahrenheit = parseFloat(fahrenheit.toFixed(1));
+
+  return fahrenheit;
 };
 
 // Do not edit below this line
