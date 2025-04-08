fc40932a24f696016c84235dd9036dbfc0dbf14f
Austin <agobby471@gmail.com>
2025-04-08 17:37:42 -0400

    Complete exercises 1-4 for javascript

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,17 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let reversedString = '';
+    let characters = string.split('');
+    let reverse = [];
 
+    for(let i = 0; i < characters.length; i++){
+        reverse[i] = characters[characters.length-i-1];
+    }
+
+    for(let i = 0; i < reverse.length; i++){
+        reversedString += reverse[i];
+    }
+
+    return reversedString;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, element, element2 = '', element3 = '', element4 = '') {
+    for(let i = 0; i < array.length; i++){
+        if(array[i] === element || array[i] === element2 || array[i] === element3 || array[i] === element4){
+            array.splice(i,1);
+            i--;
+        }
+    }
+
+    return array;
 };
 
 // Do not edit below this line
